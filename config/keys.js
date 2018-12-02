07979c9202f4ae3a99ddcec0cdfa1c4abc192ed3
ricearonin <taylor.a.rice@gmail.com>
2018-12-02 11:29:56 -0800

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // keys.js - figure out what set of credentials to return
 
-if (process.env.NODE_ENV === 'production ') {
+if (process.env.NODE_ENV === 'production') {
   //in production - return production keys
   module.exports = require('./prod');
 } else {
